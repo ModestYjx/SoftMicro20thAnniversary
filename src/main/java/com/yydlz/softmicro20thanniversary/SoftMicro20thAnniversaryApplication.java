47ef11ba9837ac452261e4df9ece487a3345575a
ModestYjx <929604665@qq.com>
2021-12-07 11:05:02 +0800

    first commit

---
+++ src/main/java/com/yydlz/softmicro20thanniversary/SoftMicro20thAnniversaryApplication.java
@@ -0,0 +1,13 @@
+package com.yydlz.softmicro20thanniversary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftMicro20thAnniversaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftMicro20thAnniversaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yydlz/softmicro20thanniversary/SoftMicro20thAnniversaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yydlz.softmicro20thanniversary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftMicro20thAnniversaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
